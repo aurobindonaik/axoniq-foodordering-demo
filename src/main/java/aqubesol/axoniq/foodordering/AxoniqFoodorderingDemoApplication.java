f3c51b501ebdaa70c2a0b0b9992ed4e4650563ca
Aurobindo Naik <aurobindonaik@aurobindos-imac.home>
2020-06-01 22:35:27 +0100

    AxonIQ food ordering demo first commit - project setup

---
+++ src/main/java/aqubesol/axoniq/foodordering/AxoniqFoodorderingDemoApplication.java
@@ -0,0 +1,13 @@
+package aqubesol.axoniq.foodordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxoniqFoodorderingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxoniqFoodorderingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aqubesol/axoniq/foodordering/AxoniqFoodorderingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package aqubesol.axoniq.foodordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxoniqFoodorderingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
